7822cb0df9e19eb30095ba895b709383a8cc362e
Paarth <paarthkothari14@gmail.com>
2023-09-24 09:33:08 +0530

    Initial Commit

---
+++ core/vege/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+
+def receipes(request):
+    if request.method == "POST":
+
+        data = request.POST
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+        receipe_image = request.FILES.get('receipe_image')
+
+        # print(receipe_name)
+        # print(receipe_description)
+        # print(receipe_image)
+
+        Receipe.objects.create(
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+            receipe_image = receipe_image
+        )
+
+        return redirect('/receipes/')
+
+    return render(request, 'receipes.html')
